a9cbd5256dca18237fd50b06a5a9f76b369af084
jaraldj <jarald.jsj@gmail.com>
2022-10-14 08:34:45 +0530

    js

--- 10-10-2022/comorins/output/foodapp/js/registration.js
+++ 10-10-2022/comorins/output/foodapp/js/registration.js
@@ -1,29 +1,78 @@
 
-function store(){
-    let name = document.getElementById("nameInput");
-    let email = document.getElementById("emailInput");
-    let password = document.getElementById("pwd");
-    let lowerCaseLetters = /[a-z]/g;
-    let upperCaseLetters = /[A-Z]/g;
-    let numbers = /[0-9]/g;
+function saveData()
+  {
 
-    if(name.value.length == 0){
-        alert("Please Enter Name")
-    }else if(email.value.length == 0){
-        alert("Please Enter email")
-    }else if(password.value.length == 0){
-        alert("Please Fill password")
-    }else if(email.value.length == 0 && password.value.length == 0){
-        alert("Please enter email and password")
-    }else if(password.value.length < 8){
-        alert("Please enter atleast 8Characters")
-    }else if(!password.value.match(numbers)){
-        alert("Please enter atleast 'one' number")
-    }else if(!password.value.match(lowerCaseLetters)){
-        alert("Please enter atleast 'one' lowercase")
-    }else if(!password.value.match(upperCaseLetters)){
-        alert("Please enter atleast 'one' uppercase")
-    }else{
-        alert("Account Created Successfully")
+    let name=document.getElementById("name").value;
+    let email=document.getElementById("email").value;
+
+    let psw=document.getElementById("psw").value;
+
+    let user_records=new Array();
+    user_records=JSON.parse(localStorage.getItem("users"))?JSON.parse(localStorage.getItem("users")):[]
+    if(user_records.some((v)=>{return v.email==email}))
+    {
+      alert("Account already exist");
     }
-}
+    else
+    {
+      user_records.push({
+      "name":name,
+      "email":email,
+      "psw":psw
+    })
+localStorage.setItem("users",JSON.stringify(user_records));
+  }
+
+  }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// let loginDetail = {
+
+// }
+
+// if(localStorage.getItem("loginDetails")){
+//     localStorage.setItem("loginDetails", JSON.stringify([]))
+// }
+
+
+// function store(){
+//     let name = document.getElementById("nameInput").value;
+//     let email = document.getElementById("emailInput").value;
+//     let password = document.getElementById("pwd").value;
+//     let lowerCaseLetters = /[a-z]/g;
+//     let upperCaseLetters = /[A-Z]/g;
+//     let numbers = /[0-9]/g;
+
+//     if(name.value.length == 0){
+//         alert("Please Enter Name")
+//     }else if(email.value.length == 0){
+//         alert("Please Enter email")
+//     }else if(password.value.length == 0){
+//         alert("Please Fill password")
+//     }else if(email.value.length == 0 && password.value.length == 0){
+//         alert("Please enter email and password")
+//     }else if(password.value.length < 8){
+//         alert("Please enter atleast 8Characters")
+//     }else if(!password.value.match(numbers)){
+//         alert("Please enter atleast 'one' number")
+//     }else if(!password.value.match(lowerCaseLetters)){
+//         alert("Please enter atleast 'one' lowercase")
+//     }else if(!password.value.match(upperCaseLetters)){
+//         alert("Please enter atleast 'one' uppercase")
+//     }else{
+//         alert("Account Created Successfully")
+//     }
+// }
--- 10-10-2022/comorins/output/foodapp/js/view.js
+++ 10-10-2022/comorins/output/foodapp/js/view.js
@@ -6,26 +6,25 @@ const queryString = window.location.search;
         
         
         let menuContainer = document.getElementById("menuList")
-
+        let cart = document.querySelectorAll(".addToCart");
+        console.log(cart);
 
         for(let i =0; i < menuSelect.length; i++){
             if(url == menuSelect[i].restaurantName){
                let menu = menuSelect[i].foodMenu
                let ele = "";
               for(let i = 0; i < menu.length; i++){
-                ele += `<div class="card mb-3" style="max-width: 540px;">
-                <div class="row no-gutters">
-    
-    
-                  <div class="col-md-8">
-                    <div class="card-body">
+                ele += `<div class="card mb-3 cardBorder">
+                <div class="row no-gutters">   
+                  <div class="col-md-9">
+                    <div class="card-body pl-0">
                       <h5 class="card-title">${menu[i].foodName}</h5>
-                      <p class="card-text">${menu[i].price}</p>
+                      <p class="card-text"><span>&#8377;</span> <span>${menu[i].price}</span></p>
                       <p class="card-text"><small class="text-muted">${menu[i].desc}</small></p>
                     </div>
                   </div>
-                  <div class="col-md-4">
-
+                  <div class="col-md-3 my-auto addBtn">
+                    <button class="btn btn-outline-success my-2 my-sm-0 ml-5 addToCart" type="submit">Add to Cart</button>
                   </div>
                 </div>
               </div>`
@@ -34,3 +33,32 @@ const queryString = window.location.search;
               }
             }
         }
+
+
+        let btn = document.getElementsByClassName("addToCart")
+        for(i = 0; i < btn.length; i++){
+          let cartBtn = btn[i]
+          cartBtn.addEventListener("click", () => {
+            let product = {
+              foodNames : event.target.parentElement.parentElement.children[0].children[0].children[0].textContent,
+              foodPrice : event.target.parentElement.parentElement.children[0].children[0].children[1].textContent,
+              totalPrice : parseInt(event.target.parentElement.parentElement.children[0].children[0].children[1].textContent),
+              quantity : 1
+            }
+            addItemToLocal(product);
+          })
+        }
+        function addItemToLocal(product){
+          if(!localStorage.getItem("productInCart")){
+            localStorage.setItem("productInCart", JSON.stringify([]))
+          }
+          let cartItem =JSON.parse(localStorage.getItem("productInCart"))
+          cartItem.push(product)
+          localStorage.setItem("productInCart", JSON.stringify(cartItem))
+          console.log(cartItem);
+        }
+        
+
+        
+
+        // <img src="${menu[i].imgUrl}" class="card-img-top" alt=""/>
